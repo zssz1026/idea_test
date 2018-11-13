d7848bea0c54bf6649ede0a44165a13f7a4b13b4
zssz1026 <zssz@20001216>
2018-11-13 21:37:00 +0800

    首次提交

---
+++ crm-dao/src/main/java/cn/sm1234/mapper/CustomerMapper.java
@@ -0,0 +1,7 @@
+package cn.sm1234.mapper;
+
+import cn.sm1234.mapper.cn.sm1234.domain.Customer;
+
+public interface CustomerMapper {
+    public void save(Customer customer);
+}
---
+++ crm-domain/src/main/java/cn/sm1234/mapper/cn/sm1234/domain/Customer.java
@@ -0,0 +1,40 @@
+package cn.sm1234.mapper.cn.sm1234.domain;
+
+public class Customer {
+    private Integer id;
+    private String name;
+    private String gender;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
